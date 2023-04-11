da1aab33ce7e7cd7412cf48067b48778837e6ce4
Liz <elizabethnino813@elizabeths-mbp.attlocal.net>
2023-04-11 17:42:25 -0500

    added note taking function

--- server.js
+++ server.js
@@ -16,4 +16,28 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+app.post("/notes", (req, res) => {
+  console.info(`${req.method} request received to save note`);
+
+  const { noteTitle, noteText } = req.body;
+
+  if (noteTitle && noteText) {
+    const newNote = {
+      noteTitle,
+      noteText,
+      review_id: uuid(),
+    };
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in posting review");
+  }
+});
+
 app.listen(PORT, () => console.log(`App is listening at http://localhost:${PORT}`));
